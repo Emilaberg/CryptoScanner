e6f7f9ee836125cebb1df5f28349d5c5bcd80bbb
Emil Åberg <emil.aberg@live.com>
2024-02-12 19:41:41 +0100

    CryptoService added to get data from db through CryptoRepo

---
+++ CryptoScanner.App/Services/CryptoScannerService.cs
@@ -0,0 +1,38 @@
+﻿using CryptoScanner.DAL.Database;
+using CryptoScanner.DAL.Models;
+using CryptoScanner.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoScanner.App.Services
+{
+    public class CryptoScannerService : ICryptoScannerService
+    {
+        internal CryptoRepo cryptoRepo { get; set; }
+
+        public CryptoScannerService(AppDbContext context)
+        {
+            cryptoRepo = new(context);
+
+        }
+
+        public async Task<List<CryptoModel>> GetAllCryptoAsync()
+        {
+            return await cryptoRepo.GetAllAsync();   
+
+        }
+
+        public CryptoModel GetCrypto(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCrypto(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CryptoScanner.App/Services/ICryptoScannerService.cs
@@ -0,0 +1,18 @@
+﻿using CryptoScanner.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoScanner.App.Services
+{
+    public interface ICryptoScannerService
+    {
+        public Task<List<CryptoModel>> GetAllCryptoAsync();
+        public CryptoModel GetCrypto(string name);
+
+        public void UpdateCrypto(string name);
+
+    }
+}
