cada70e223de2b16cd6d3afb1be34c4d1965d427
Emil Åberg <61052905+Emilaberg@users.noreply.github.com>
2024-02-12 14:31:46 +0100

    Add project files.

---
+++ CryptoScanner.UI/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CryptoScanner.UI.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+
+        public void OnGet()
+        {
+        }
+    }
+}
